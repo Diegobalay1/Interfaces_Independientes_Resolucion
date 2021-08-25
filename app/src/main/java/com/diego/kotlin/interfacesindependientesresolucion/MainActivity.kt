303e8305dc0cec2ee2d4b28c843552489647a755
balay <dlolhd@gmail.com>
2021-08-25 09:42:03 +0200

    añadido intent implicito que nos lleva a la pagina imdb, en este caso del regreso al futuro

--- app/src/main/java/com/diego/kotlin/interfacesindependientesresolucion/MainActivity.kt
+++ app/src/main/java/com/diego/kotlin/interfacesindependientesresolucion/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.diego.kotlin.interfacesindependientesresolucion
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imdbRegresoFuturo = "tt0088763/"
+        val botonImdb = findViewById<Button>(R.id.btnImdb)
+        botonImdb.setOnClickListener {
+            val intWebIMDB = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.imdb.com/title/$imdbRegresoFuturo"))
+            startActivity(intWebIMDB)
+        }
+
+
+
     }
 }
